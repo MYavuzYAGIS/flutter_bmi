3b92fcec5c72bd981b92dea04a17867feffc7389
M. Yavuz YAGIS <myagis16@ku.edu.tr>
2020-11-17 15:38:32 +0300

    create extracted class for icons

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E32);
+const bottomContainerColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -25,25 +28,24 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      color: Color(0xFF1D1E32),
-                    ),
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                    cardChild: iconContent(),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
-                    colour: Color(0xFF1D1E32),
+                    colour: activeCardColor,
+                    cardChild: iconContent(),
                   ),
-                )
+                ),
               ],
             ),
           ),
+
           Expanded(
             child: ReusableCard(
-              colour: Color(0xFF1D1E32),
+              colour: activeCardColor,
             ),
           ),
           Expanded(
@@ -51,12 +53,12 @@ class _InputPageState extends State<InputPage> {
             children: <Widget>[
               Expanded(
                 child: ReusableCard(
-                  colour: Color(0xFF1D1E32),
+                  colour: activeCardColor,
                 ),
               ),
               Expanded(
                 child: ReusableCard(
-                  colour: Color(0xFF1D1E32),
+                  colour: activeCardColor,
                 ),
               ),
             ],
@@ -65,7 +67,7 @@ class _InputPageState extends State<InputPage> {
           //Below is the bottom row.
           //  width : double.infinity ekrean boyutu ne olurssa olsun tamamini kapla demek.
           Container(
-            color: Color(0xFFEB1555),
+            color: bottomContainerColor,
             margin: EdgeInsets.only(top: 11.0),
             width: double.infinity,
             height: bottomContainerHeight,
@@ -79,11 +81,37 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class iconContent extends StatelessWidget {
+  iconContent({this.icon, this.label});
+  final IconData icon;
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          FontAwesomeIcons.mars,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          'MALE',
+          style: TextStyle(fontSize: 18.0, color: Color(0xFF8F8E98)),
+        )
+      ],
+    );
+  }
+}
+
 // Starting Creating the Cards on the UI
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour});
+  ReusableCard({@required this.colour, this.cardChild});
   final Color colour;
-
+  //we are creating new reausabble card as widget
+  final Widget cardChild;
   //final makes the property immutable.
 
   //widget olustururken 6 tane card icin 6 blok container kod yazdik. buna gerek olmasin diye de
@@ -95,6 +123,7 @@ class ReusableCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
