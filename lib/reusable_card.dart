fdd9cc581f260f1af5b69a0ab5de32f6d92f00a2
M. Yavuz YAGIS <myagis16@ku.edu.tr>
2020-11-17 21:41:16 +0300

    refactor code to have the ability to accept GestureDetection function as parameter

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour, this.cardChild});
+  ReusableCard({@required this.colour, this.cardChild, this.onPress});
   final Color colour;
   //we are creating new reausabble card as widget
   final Widget cardChild;
   //final makes the property immutable.
 
+  final Function onPress;
   //widget olustururken 6 tane card icin 6 blok container kod yazdik. buna gerek olmasin diye de
   // extract widgete basarak otomatik olarak classa donusturduk.
   // bu blok kodu ureten flutter oldu.
@@ -15,12 +16,15 @@ class ReusableCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: colour,
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: cardChild,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: colour,
+        ),
       ),
     );
   }
